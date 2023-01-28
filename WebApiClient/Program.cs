8610db06f11bd0df6d2e27dafd117f812a51f8c7
João Bruno <jbrunomf@outlook.com>
2023-01-28 19:25:07 -0400

    Atualizando, adicionando README.md

--- WebApiClient/Program.cs
+++ WebApiClient/Program.cs
@@ -11,15 +11,21 @@ client.DefaultRequestHeaders.Accept.Add(
     new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
 client.DefaultRequestHeaders.Add("User-Agent", ".Net Foundation Repository Reporter");
 
-await ProcessRepositoriesAsync(client);
+var repositories = await ProcessRepositoriesAsync(client);
 
-static async Task ProcessRepositoriesAsync(HttpClient client)
+foreach (var repo in repositories)
+{
+    Console.WriteLine($"Name: {repo.Name}");
+    Console.WriteLine($"Homepage: {repo.Homepage}");
+    Console.WriteLine($"GitHub: {repo.GitHubHomeUrl}");
+    Console.WriteLine($"Description: {repo.Description}");
+    Console.WriteLine();
+}
+
+static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient client)
 {
     await using Stream stream = await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
     var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
 
-    foreach (var repository in repositories ?? Enumerable.Empty<Repository>())
-    {
-        Console.Write(repository.name);
-    }
+    return repositories ?? new();
 }
---
+++ WebApiClient/README.md
@@ -0,0 +1,13 @@
+﻿# WebAPI Client
+Esse projeto foi desenvolvido para fins educacionais de aprendizado.
+Trata-se de um Console Application.
+
+Objetivo:
+- Consumir a API do Github, listando os repositórios da .NET Foundation.
+
+Tecnologias utilizadas:
+- C#
+- .NET
+
+Exemplo:
+![img.png](img.png)
